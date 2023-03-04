fd9413773e98e29d708d49bec01f47c692e1e330
Barberousse93 <mobile.barberousse@gmail.com>
2023-03-04 12:47:06 +0100

    Annulation event change sur input listes

--- scripts/index.js
+++ scripts/index.js
@@ -19,23 +19,23 @@ const filterTags = {
   ust: []
 }
 
-//* *********************************************************** *//
-// Ajout de l'event change sur les input des listes pour gérér les effacements de zone
+// //* *********************************************************** *//
+// // Ajout de l'event change sur les input des listes pour gérér les effacements de zone
 const inputUst = document.querySelector('#inputUst')
-inputUst.addEventListener('change', () => {
-  ListeUstensiles(filteredRecipes)
-})
+// inputUst.addEventListener('change', () => {
+//   ListeUstensiles(filteredRecipes)
+// })
 
 const inputIng = document.querySelector('#inputIng')
-inputIng.addEventListener('change', () => {
-  ListeIngredients(filteredRecipes)
-})
+// inputIng.addEventListener('change', () => {
+//   ListeIngredients(filteredRecipes)
+// })
 
 const inputApp = document.querySelector('#inputApp')
-inputApp.addEventListener('change', () => {
-  ListeApareils(filteredRecipes)
-})
-//* *********************************************************** *//
+// inputApp.addEventListener('change', () => {
+//   ListeApareils(filteredRecipes)
+// })
+// //* *********************************************************** *//
 
 //* *********************************************************** *//
 // Ajout des Event 'click' sur les entêtes de liste de filtres
